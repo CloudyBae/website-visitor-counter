15466717d9e3d0b8d03e4aa0f119dac5127fbbba
Yoojin93 <yoojinbae93@gmail.com>
2023-04-14 14:29:51 -0500

    terraform

---
+++ dynamodb.tf
@@ -0,0 +1,13 @@
+resource "aws_dynamodb_table" "website-count-table" {
+    name = "ViewCount"
+    billing_mode = "PROVISIONED"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "Count"
+    
+    attribute {
+        name = "Count"
+        type = "N"
+    }
+
+}
